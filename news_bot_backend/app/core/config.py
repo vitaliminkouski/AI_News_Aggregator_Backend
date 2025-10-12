3dd4b056d21d221fc4a49c131d88a0e47626fc3b
vitaliminkouski <vitaliminkouski@gmail.com>
2025-10-13 00:00:42 +0300

    packed project into Docker container

--- news_bot_backend/app/core/config.py
+++ news_bot_backend/app/core/config.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
@@ -20,6 +21,10 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     DATABASE_URL: Optional[str] = None
     ASYNC_DATABASE_URL: Optional[str] = None
+
+    SECRET_KEY: str = Field(..., min_length=32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ALGORITHM: str = "HS256"
 
     class Config:
         env_file = ".env"
--- news_bot_backend/app/main.py
+++ news_bot_backend/app/main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
-from app.api.v1.testroutes import router as test_router
+from .api.v1.testroutes import router as test_router
+
+
 app=FastAPI()
 app.include_router(test_router)
 
