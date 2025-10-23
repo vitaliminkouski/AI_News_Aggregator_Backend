da7a56882b3b9e1a4d05d9ebb0e8e4cddcef5c2d
vitaliminkouski <vitaliminkouski@gmail.com>
2025-10-23 23:17:31 +0300

    Some changes to Database

--- news_bot_backend/app/models/permission.py
+++ news_bot_backend/app/models/permission.py
@@ -6,4 +6,4 @@ from app.db.database import Base
 class Permission(Base):
     __tablename__ = 'Permission'
     id=Column(Integer, primary_key=True, autoincrement=True, index=True, unique=True)
-    name=Column(String(50), unique=True, nullable=False)
+    name=Column(String(10), unique=True, nullable=False)
--- news_bot_backend/app/models/refresh_token.py
+++ news_bot_backend/app/models/refresh_token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime, String
 from sqlalchemy.orm import relationship
 
 from app.db.database import Base
@@ -9,7 +9,7 @@ class RefreshToken(Base):
     __tablename__ = "RefreshToken"
 
     id=Column(Integer, primary_key=True, autoincrement=True, index=True)
-    hashed_token=Column(Text, nullable=False)
+    hashed_token=Column(String(255), nullable=False)
 
     user_id=Column(Integer, ForeignKey("User.id", ondelete="CASCADE", onupdate="CASCADE"))
     user=relationship(User)
